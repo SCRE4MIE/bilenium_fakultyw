97509b8c9a395b2546699ef68af4b6cf89c4cf6f
Mateusz Baran <scgfxpl@onet.pl>
2022-04-07 16:50:48 +0200

    endpoint rejestracja, logowanie: email + hasło

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,4 +9,5 @@ from django.urls import path
 app_name = 'api_v1'
 urlpatterns = [
     path('auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls'))
 ]
