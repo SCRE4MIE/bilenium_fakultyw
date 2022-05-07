be5cb24aee932ad54ff6dee3eb959bbc0b73daf7
Mateusz Baran <scgfxpl@onet.pl>
2022-05-07 22:46:57 +0200

    walidacja danych: max 3 psy na spacerze i 5 psow jednego dnia

--- backend/api/forms.py
+++ backend/api/forms.py
@@ -3,15 +3,24 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 from api.models import Walk
+from datetime import date
 
 
 class M2MDogsLimit(forms.ModelForm):
+    """Dogs limits, max 3 dogs per walk"""
     class Meta:
         model = Walk
         fields = '__all__'
 
     def clean(self):
         dogs = self.cleaned_data.get('dogs')
-        if dogs and dogs.count() > 3:
+        if dogs and dogs.count() > 3:  # max 3  dogs per walk
             raise ValidationError('Mogą być tylko 3 psy!')
+
+        trainer = self.cleaned_data.get('trainer')
+        today = date.today()
+        walks = trainer.walk_set.filter(date__day=today.day, date__month=today.month, date__year=today.year)
+        if walks.count() > 5:  # max 5 walks per day
+            raise ValidationError('Jednego dnia może być tylko 5 slotów spacerowych!')
+
         return self.cleaned_data
--- backend/api/models.py
+++ backend/api/models.py
@@ -117,3 +117,5 @@ class Walk(models.Model):
         null=False,
         blank=False,
     )
+
+
