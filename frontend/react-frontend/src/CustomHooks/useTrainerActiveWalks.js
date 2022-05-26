e8df21d30eaa022919655cfc5238c95d2273cfec
Mateusz <traczyk.mateusz76@gmail.com>
2022-05-26 14:43:03 +0200

    hook trainer walks

---
+++ frontend/react-frontend/src/CustomHooks/useTrainerActiveWalks.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { act } from 'react-dom/test-utils';
+import instance from '../axios';
+import requests from '../requests';
+
+const useTrainerActiveWalks = () => {
+
+  const dayjs = require("dayjs");
+  const today = dayjs();
+
+  const [walks, setWalks] = useState([]);
+
+  let activeWalks = walks.filter((e) => {
+    const start = dayjs(e.date);
+    const end = dayjs(e.date_end);
+    return start <= today && end >= today;
+  });
+
+  const unique = [];
+  const map = new Map();
+
+  for(const e of activeWalks){
+    if(!map.has(e.id)){
+      map.set(e.id, true);
+      unique.push(e);
+    }
+  }
+
+  useEffect(() => {
+    instance.get(requests.getWalks)
+    .then(response => setWalks(response.data));
+  }, [])
+
+  return unique;
+}
+
+export default useTrainerActiveWalks;
